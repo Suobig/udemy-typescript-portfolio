0936dd0e6662bab06bfbe2c844f8f30cc7c573cd
Попов Антон Сергеевич <ASergeevPopov@sberbank.ru>
2021-08-12 16:05:57 +0300

    User search (union types)

---
+++ rts/src/state/UserSearch.tsx
@@ -0,0 +1,53 @@
+import { FC, useState, useMemo } from 'react'
+
+const users = [
+  { name: 'Sarah', age: 20 },
+  { name: 'Alex', age: 21 },
+  { name: 'Michael', age: 19 },
+]
+
+const UserSearch: FC = () => {
+  const [name, setName] = useState('')
+  const [searchResult, setSearchResult] = useState<
+    { name: string; age: number } | null | undefined
+  >(null)
+
+  const onSearch = () => {
+    const foundUser = users.find((user) => user.name.match(name))
+    if (!foundUser) {
+      setSearchResult(null)
+    } else {
+      setSearchResult(foundUser)
+    }
+  }
+
+  const resultComponent = useMemo(() => {
+    if (!searchResult) {
+      return <div>No results</div>
+    } else {
+      const { name, age } = searchResult
+      return (
+        <div>
+          <div>{name}</div>
+          <div>{age}</div>
+        </div>
+      )
+    }
+  }, [searchResult])
+
+  return (
+    <div>
+      <h3>User Search</h3>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={onSearch}>Search</button>
+      {searchResult && <h3>Search result:</h3>}
+      {resultComponent && <div>{resultComponent}</div>}
+    </div>
+  )
+}
+
+export default UserSearch
