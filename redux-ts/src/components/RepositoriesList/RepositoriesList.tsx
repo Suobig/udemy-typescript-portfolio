10e72349f6bcf6e33307ec40728a4ed72b68b614
Попов Антон Сергеевич <ASergeevPopov@sberbank.ru>
2021-09-04 15:53:22 +0300

    displayed found repositories

--- redux-ts/src/components/RepositoriesList/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList/RepositoriesList.tsx
@@ -1,8 +1,9 @@
 import React, { FC, useState } from 'react'
 
-import { useActions } from '../hook/useActions'
+import { useActions } from '../hooks/useActions'
 
 import s from './RepositoriesList.module.sass'
+import useTypedSelector from '../hooks/useTypedSelector'
 
 interface IProps {}
 
@@ -14,6 +15,27 @@ const RepositoriesList: FC<IProps> = () => {
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     searchRepositories(term)
+  }
+
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories,
+  )
+
+  const Results: FC = () => {
+    if (error) {
+      return <h3>{error}</h3>
+    }
+    if (loading) {
+      return <h3>Loading...</h3>
+    }
+
+    return (
+      <ul>
+        {data.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+    )
   }
 
   return (
@@ -28,6 +50,7 @@ const RepositoriesList: FC<IProps> = () => {
         />
         <button type="submit">Search</button>
       </form>
+      <Results />
     </div>
   )
 }
