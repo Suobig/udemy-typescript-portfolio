089fca804b66d559145a01ad87c0f3062c2af641
Попов Антон Сергеевич <ASergeevPopov@sberbank.ru>
2021-09-06 21:57:13 +0300

    started bundling using esbuild

---
+++ jbook/src/components/App.tsx
@@ -0,0 +1,50 @@
+import { FC, useState, useEffect, useRef } from 'react'
+import * as esbuild from 'esbuild-wasm'
+import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin'
+
+const App: FC = () => {
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+  const ref = useRef<esbuild.Service>()
+
+  const onSubmit = async () => {
+    if (!ref.current) {
+      return
+    }
+
+    const result = await ref.current.build({
+      bundle: true,
+      write: false,
+      entryPoints: ['index.js'],
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    })
+    setCode(result.outputFiles[0].text)
+  }
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: './esbuild.wasm',
+    })
+  }
+
+  useEffect(() => {
+    startService()
+  }, [])
+
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+      <div>
+        <button onClick={onSubmit}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  )
+}
+
+export default App
